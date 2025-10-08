a706acddd6543b9965fcc8f7675642561a1ce0fe
Jagadeeswara Reddy <njagadeeswarareddy@deloitte.com>
2025-10-08 14:24:57 +0530

    upload file in repo

--- Project/first-tf-project/providers.tf
+++ Project/first-tf-project/providers.tf
@@ -29,20 +29,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# Now we can simple deploy the S3 bucket 
-resource "random_id" "bucket_suffix" {
-    byte_length = 6
-}
 
-resource "aws_s3_bucket" "example_bucket" {
-    bucket = "example_bucket-${random_id.bucket_suffix.id}"
-  
-}
-
-output "bucket_name" {
-    value = aws_s3_bucket.example_bucket.bucket
-  
-}
 
 
 /*
